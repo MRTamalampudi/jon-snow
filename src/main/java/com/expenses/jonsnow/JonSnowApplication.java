13151cb885fffc44bbfd2854162c02cab1dde833
MRTamalampudi <89578665+MRTamalampudi@users.noreply.github.com>
2023-09-23 04:14:05 +0530

    init

---
+++ src/main/java/com/expenses/jonsnow/JonSnowApplication.java
@@ -0,0 +1,13 @@
+package com.expenses.jonsnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonSnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JonSnowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expenses/jonsnow/JonSnowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expenses.jonsnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonSnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
