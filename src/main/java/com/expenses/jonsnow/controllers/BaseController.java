e5e2082f6575e0e8530e4dcdd6c2994fa661d0a2
MRTamalampudi <89578665+MRTamalampudi@users.noreply.github.com>
2024-01-22 20:45:40 +0530

    feat: add SplitBillGroupMapper

--- src/main/java/com/expenses/jonsnow/controllers/BaseController.java
+++ src/main/java/com/expenses/jonsnow/controllers/BaseController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.AbstractController;
 
 import java.util.List;
 
--- src/main/java/com/expenses/jonsnow/mapper/SplitBillGroupMapper.java
+++ src/main/java/com/expenses/jonsnow/mapper/SplitBillGroupMapper.java
@@ -1,9 +1,24 @@
 package com.expenses.jonsnow.mapper;
 
 import com.expenses.jonsnow.dto.SplitBillGroupDTO;
+import com.expenses.jonsnow.dto.SplitBillGroupMemberDTO;
 import com.expenses.jonsnow.model.SplitBillGroup;
+import com.expenses.jonsnow.model.SplitBillGroupMember;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface SplitBillGroupMapper extends BaseMapper<SplitBillGroup, SplitBillGroupDTO, SplitBillGroupDTO>{
+
+    @Override
+    @Mapping(target = "memberList", source = "memberList",qualifiedByName = "mapMemberListToDTOList")
+    SplitBillGroupDTO mapEntityToDTO(SplitBillGroup splitBillGroup);
+
+    @Named("mapMemberListToDTOList")
+    List<SplitBillGroupMemberDTO> mapMemberListToDTOList(List<SplitBillGroupMember> splitBillGroupMembers);
+    @Mapping(target = "group",ignore = true)
+    SplitBillGroupMemberDTO mapMembertoDTO(SplitBillGroupMember member);
 }
